3b649fe1510c61a1b64b2c92a63e37309eda49fb
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-09-29 10:24:47 +0600

    initialization

---
+++ src/components/Excercise/Excercise.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Excercise.css';
+
+const Excercise = () => {
+    return (
+        <div className='excercise'>
+            <div className="excercise-container">
+                <h1>Today's excercise</h1>
+            </div>
+            <div className="activity-container">
+                <h1>Activity section</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Excercise;
